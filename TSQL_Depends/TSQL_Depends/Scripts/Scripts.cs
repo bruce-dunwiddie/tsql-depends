89d3cc6a037dbae9fcffb7063c61399b2f57b1c2
Bruce Dunwiddie <bruce.dunwiddie@selectquote.com>
2019-11-12 23:20:28 -0600

    Added resource loader logic for loading script text from embedded resources that is also compatible with .Net Standard build.

---
+++ TSQL_Depends/TSQL_Depends/Scripts/Scripts.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSQL.Depends.Scripts
+{
+	internal static class Script
+	{
+		public static string GetColumns
+		{
+			get
+			{
+				return GetScript("GetColumns.sql");
+			}
+		}
+
+		private static string GetScript(string name)
+		{
+			using (StreamReader reader = new StreamReader(
+				Assembly
+					.GetExecutingAssembly()
+					.GetManifestResourceStream(
+						typeof(Script).Namespace + "." + name)))
+			{
+				return reader.ReadToEnd();
+			}
+		}
+	}
+}
---
+++ TSQL_Depends/Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("781c92ea-3bc4-4421-9a3c-66ec1516c7a9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
