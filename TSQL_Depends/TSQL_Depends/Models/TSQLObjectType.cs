c0689a6e900d2ba0b32a6a34554ca36848d5da94
Aλexey I.m2strng4dtwrld <inredikawb@gmail.com>
2019-11-18 04:09:42 +0300

    refactor: make comments as summary in TSQLObjectType

--- TSQL_Depends/TSQL_Depends/Models/TSQLObjectType.cs
+++ TSQL_Depends/TSQL_Depends/Models/TSQLObjectType.cs
@@ -1,74 +1,153 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace TSQL.Depends.Models
 {
 	internal enum TSQLObjectType
 	{
-		/*
-		AF = Aggregate function (CLR)
-		C = CHECK constraint
-		D = DEFAULT (constraint or stand-alone)
-		F = FOREIGN KEY constraint
-		FN = SQL scalar function
-		FS = Assembly (CLR) scalar-function
-		FT = Assembly (CLR) table-valued function
-		IF = SQL inline table-valued function
-		IT = Internal table
-		P = SQL Stored Procedure
-		PC = Assembly (CLR) stored-procedure
-		PG = Plan guide
-		PK = PRIMARY KEY constraint
-		R = Rule (old-style, stand-alone)
-		RF = Replication-filter-procedure
-		S = System base table
-		SN = Synonym
-		SO = Sequence object
-		Applies to: SQL Server 2012 through SQL Server 2014.
-		SQ = Service queue
-		TA = Assembly (CLR) DML trigger
-		TF = SQL table-valued-function
-		TR = SQL DML trigger
-		TT = Table type
-		U = Table (user-defined)
-		UQ = UNIQUE constraint
-		V = View
-		X = Extended stored procedure
-		*/
+        /// <summary>
+        /// AF = Aggregate function (CLR)
+        /// </summary>
+        CLRAggregateFunction,
 
-		// undocumented
-		// SP = Security Policy
-
-		CLRAggregateFunction,
+        /// <summary>
+        /// C = CHECK constraint
+        /// </summary>
 		CheckConstraint,
+
+        /// <summary>
+        /// D = DEFAULT (constraint or stand-alone)
+        /// </summary>
 		Default,
+
+        /// <summary>
+        /// F = FOREIGN KEY constraint
+        /// </summary>
 		ForeignKey,
+
+        /// <summary>
+        /// FN = SQL scalar function
+        /// </summary>
 		ScalarFunction,
+
+        /// <summary>
+        /// FS = Assembly (CLR) scalar-function
+        /// </summary>
 		CLRScalarFunction,
+
+        /// <summary>
+        /// FT = Assembly (CLR) table-valued function
+        /// </summary>
 		CLRTableValuedFunction,
+
+        /// <summary>
+        /// IF = SQL inline table-valued function
+        /// </summary>
 		InlineTableValuedFunction,
+
+        /// <summary>
+        /// IT = Internal table
+        /// </summary>
 		InternalTable,
+
+        /// <summary>
+        /// P = SQL Stored Procedure
+        /// </summary>
 		StoredProcedure,
+
+        /// <summary>
+        /// PC = Assembly (CLR) stored-procedure
+        /// </summary>
 		CLRStoredProcedure,
+
+        /// <summary>
+        /// PG = Plan guide
+        /// </summary>
 		PlanGuide,
+
+        /// <summary>
+        /// PK = PRIMARY KEY constraint
+        /// </summary>
 		PrimaryKey,
+
+        /// <summary>
+        /// R = Rule (old-style, stand-alone)
+        /// </summary>
 		Rule,
+
+        /// <summary>
+        /// RF = Replication-filter-procedure
+        /// </summary>
 		ReplicationFilter,
+
+        /// <summary>
+        /// S = System base table
+        /// </summary>
 		SystemTable,
+
+        /// <summary>
+        /// SN = Synonym
+        /// </summary>
 		Synonym,
+
+        /// <summary>
+        /// SO = Sequence object
+        /// </summary>
 		Sequence,
+
+        #region Applies to: SQL Server 2012 through SQL Server 2014.
+
+        /// <summary>
+        /// SQ = Service queue
+        /// </summary>
 		ServiceQueue,
+
+        /// <summary>
+        /// TA = Assembly (CLR) DML trigger
+        /// </summary>
 		CLRTrigger,
+
+        /// <summary>
+        /// TF = SQL table-valued-function
+        /// </summary>
 		TableValuedFunction,
+
+        /// <summary>
+        /// TR = SQL DML trigger
+        /// </summary>
 		Trigger,
+
+        /// <summary>
+        /// TT = Table type
+        /// </summary>
 		TableType,
+
+        /// <summary>
+        /// U = Table (user-defined)
+        /// </summary>
 		Table,
+
+        /// <summary>
+        /// UQ = UNIQUE constraint
+        /// </summary>
 		UniqueConstraint,
+
+        /// <summary>
+        /// V = View
+        /// </summary>
 		View,
+
+        /// <summary>
+        /// X = Extended stored procedure
+        /// </summary>
 		ExtendedStoredProcedure,
+
+        #endregion
+
+        #region undocumented
+
+        /// <summary>
+        /// SP = Security Policy
+        /// </summary>
 		SecurityPolicy
+
+        #endregion
 	}
 }
